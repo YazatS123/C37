4aa053bc4a6aa63bb377545c98a0651afbfdf60c
YazatS123 <77256681+YazatS123@users.noreply.github.com>
2021-03-21 21:23:21 -0500

    Add files via upload

---
+++ C37/js/Game.js
@@ -0,0 +1,35 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+  play(){
+    form.hide();
+    textSize(30);
+    text("The game is starting!", 100, 160);
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount()
+      }
+      form = new Form()
+      form.display();
+    }
+  }
+}
